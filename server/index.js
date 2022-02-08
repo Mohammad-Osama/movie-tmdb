37b3012e47a9aec58feec4788b3ecb04722d07f1
Mohammad <moh.osama84x@gmail.com>
2022-02-08 05:16:43 +0200

    edit 4 setupProxy.js,code edit in server

--- server/index.js
+++ server/index.js
@@ -22,11 +22,11 @@ app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,"../public","index.html"))
 });
  */
-
-app.use(express.static(path.resolve(__dirname, '../build')));
-/* app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
-}); */
+if  (process.env.NODE_ENV === 'production')
+    {app.use(express.static(path.resolve(__dirname, '../build')));
+      app.get('*', (req, res) => {
+         res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+        }); }
 
 
 app.listen(PORT, () => {
--- setupProxy.js
+++ setupProxy.js
@@ -2,6 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-      createProxyMiddleware('/flask/**', { target: "http://localhost:3001" })
+      createProxyMiddleware('**', { target: "http://localhost:3001" })
     );
   };
