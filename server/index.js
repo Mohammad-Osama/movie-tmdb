a63cc6f44a746c9a3670b99ec141f4981c75699a
Mohammad <moh.osama84x@gmail.com>
2022-02-09 04:35:41 +0200

    edit 10

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const express = require("express");
 
-const PORT = process.env.PORT || 5000;
+ //const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -29,8 +29,8 @@ if  (process.env.NODE_ENV === 'production')
         }); }
 
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+   //console.log(`Server listening on ${PORT}`);
 });
 
 const url = "https://api.themoviedb.org/3/movie/"
