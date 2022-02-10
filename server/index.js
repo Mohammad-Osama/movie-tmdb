32dbd9fdcb70dd956eb870d99f2fc5d632971ba8
Mohammad <moh.osama84x@gmail.com>
2022-02-10 03:05:02 +0200

    edit 13

--- server/index.js
+++ server/index.js
@@ -23,9 +23,9 @@ app.get("/", (req,res) => {
 });
  */
 if  (process.env.NODE_ENV === 'production')
-    {app.use(express.static(path.join(__dirname, '../build')));
-      app.get('*', (req, res) => {
-         res.sendFile(path.join(__dirname, '../build/index.html'));
+    {app.use(express.static(path.join(__dirname, '..', 'build')));
+      app.get('/*', (req, res) => {
+         res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
         }); }
 
 
